4010f95602c6de65fe9e3853d801fa2f431c0662
Al0w1n <f20221186@goa.bits-pilani.ac.in>
2024-07-19 12:27:12 +0530

    12

--- db_proxy_setup.sh
+++ db_proxy_setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Azure-specific settings
 AZURE_REGION=eastus  # Set your Azure region
 DB_NAME=crowdsourcedb
 AZURE_AUTH=azure-auth-file.json  # Path to your Azure service principal credentials
@@ -14,19 +15,15 @@ az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET --tenan
 az account set --subscription $AZURE_SUBSCRIPTION_ID
 
 # Create an Azure SQL Database
-az sql db create --resource-group $AZURE_RESOURCE_GROUP --server $AZURE_SQL_SERVER --name $DB_NAME --service-objective S0
+az sql db create --resource-group $RESOURCE_GROUP --server $AZURE_SQL_SERVER --name $DB_NAME --service-objective S0
 
 # Configure the Azure SQL Database firewall to allow access
-az sql server firewall-rule create --resource-group $AZURE_RESOURCE_GROUP --server $AZURE_SQL_SERVER --name AllowYourIp --start-ip-address YOUR_IP --end-ip-address YOUR_IP
-
-# Download and configure the Azure SQL proxy
-wget https://github.com/GoogleCloudPlatform/cloudsql-proxy/releases/download/v1.28.0/cloud_sql_proxy.linux.amd64 -O cloud_sql_proxy
-chmod +x cloud_sql_proxy
-nohup ./cloud_sql_proxy -dir=./cloudsql -instances=${AZURE_SQL_SERVER}:${AZURE_REGION}:${DB_NAME}=tcp:5432 &
-sleep 25s
-cat nohup.out
+az sql server firewall-rule create --resource-group $RESOURCE_GROUP --server $AZURE_SQL_SERVER --name AllowYourIp --start-ip-address $YOUR_IP --end-ip-address $YOUR_IP
 
 # Export Azure credentials
 export AZURE_APPLICATION_CREDENTIALS=${HOME}/$AZURE_AUTH
 
 echo $AZURE_APPLICATION_CREDENTIALS
+
+# Example command to connect to Azure SQL Database using sqlcmd (make sure to install sqlcmd tool)
+# sqlcmd -S tcp:${AZURE_SQL_SERVER}.database.windows.net,1433 -d $DB_NAME -U $AZURE_SQL_ADMIN_USER -P $AZURE_SQL_ADMIN_PASSWORD -G
--- packages/scripts/set_env.sh
+++ packages/scripts/set_env.sh
@@ -7,3 +7,7 @@ export COMPOSER_ENV_NAME="ekstepcomposer-test"  # Azure equivalent service/envir
 export AZURE_PROJECT="ekstepspeechrecognition"  # Name of your project (if applicable)
 export DB_REGION="eastus"                # Azure region for your database
 export DB_NAME="crowdsourcedb"           # Database name (if applicable)
+export AZURE_SQL_SERVER="your-sql-server"  # Replace with your Azure SQL Server name
+export AZURE_SQL_ADMIN_USER="your-admin-user"  # Replace with your Azure SQL admin user
+export AZURE_SQL_ADMIN_PASSWORD="your-admin-password"  # Replace with your Azure SQL admin password
+export YOUR_IP="your-ip-address"  # Replace with your actual IP address
